9a89d8d04f8437ea137dee0ebe467e66830a2d0f
Sebas <sebastian.ituarte@gmail.com>
2020-10-12 15:42:54 -0300

    Create hmac.test.js

---
+++ test/clouders/hmac.test.js
@@ -0,0 +1,12 @@
+import chai from 'chai';
+import { listarClouders } from '../../use-cases/clouders';
+
+chai.should();
+
+// TEST #listarClouders()
+describe('#listarClouders()', () => {
+  it('Deberia retornar todos los clouders que estan en la BD actualmente', () => {
+    //const clouders = await listarClouders();
+    console.log('Ver que tiene clouders - ');
+  });
+});
